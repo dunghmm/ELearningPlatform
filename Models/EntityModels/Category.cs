efdfeeda4a8b4d3c01f35442e0c87114f8715e5f
MinhMinh2609 <minh955371@gmail.com>
2025-12-11 11:50:59 +0700

    EntityBaseModels hết các bảng

--- Models/EntityModels/Category.cs
+++ Models/EntityModels/Category.cs
@@ -3,17 +3,9 @@ using System.Collections.Generic;
 
 namespace ELearningPlatform.Models.EntityModels;
 
-public partial class Category
+public partial class Category : EntityBaseModel
 {
-    public int Id { get; set; }
-
     public string Name { get; set; } = null!;
-
-    public DateTime CreatedAt { get; set; }
-
-    public DateTime UpdatedAt { get; set; }
-
-    public bool DeleteFlag { get; set; }
 
     public virtual ICollection<CourseCategory> CourseCategories { get; set; } = new List<CourseCategory>();
 }
--- Models/EntityModels/CourseCategory.cs
+++ Models/EntityModels/CourseCategory.cs
@@ -3,19 +3,11 @@ using System.Collections.Generic;
 
 namespace ELearningPlatform.Models.EntityModels;
 
-public partial class CourseCategory
+public partial class CourseCategory : EntityBaseModel
 {
-    public int Id { get; set; }
-
     public int CourseId { get; set; }
 
     public int CategoryId { get; set; }
-
-    public DateTime CreatedAt { get; set; }
-
-    public DateTime UpdatedAt { get; set; }
-
-    public bool DeleteFlag { get; set; }
 
     public virtual Category Category { get; set; } = null!;
 
